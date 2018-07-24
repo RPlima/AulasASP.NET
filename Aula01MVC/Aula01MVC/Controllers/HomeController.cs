24c957e0bd226afb4bc781f353e97d2c17bfd9d7
Raphael de Lima Santana <raphaeltwins@hotmail.com>
2018-07-24 11:13:49 -0300

    Aula 1: Criação da solution com MVC + Básico da Controller(ActionResult) + Básico View HTML

---
+++ Aula01MVC/Aula01MVC/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aula01MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        // Métodos dentro de um Controller são chamados de Action
+        public string Index()
+        {
+            return "<h1>Meu Jhonson!!<h1>";
+        }
+
+        public ActionResult PaginaUm()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Aula01MVC/Aula01MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Aula01MVC.MvcApplication" Language="C#" %>
